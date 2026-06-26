9dfb63fb04e3140691ec5528cfebe10fd4ef07ae
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-26 15:13:25 -0500

    feat: add background-2 with layered images and bottom button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import bg1 from './assets/background-1.jpg'
+import bg2 from './assets/background-2.webp'
+import frontScene from './assets/front-scene.webp'
 
 function App() {
   return (
-    <div className="w-full h-screen overflow-hidden">
-      <img src={bg1} alt="background" className="w-full h-full object-cover" />
+    <div className="relative w-full h-screen overflow-hidden">
+      <img src={bg1} alt="background-1" className="absolute inset-0 w-full h-full object-cover" />
+      <img src={bg2} alt="background-2" className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[190%] " />
+      <img src={frontScene} alt="background-2" className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[190%] " />
+      
     </div>
   )
 }
