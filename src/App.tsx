d8aa75759473d84f453129e1df20a5124d902ac2
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-26 15:20:03 -0500

    feat(ui): add layered cloud assets to landing scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import bg1 from './assets/background-1.jpg'
 import bg2 from './assets/background-2.webp'
 import frontScene from './assets/front-scene.webp'
+import cloud1 from './assets/cloud-1.webp'
+import cloud2 from './assets/cloud-2.webp'
+import cloud3 from './assets/cloud-3.webp'
+import cloud4 from './assets/cloud-4.webp'
+import petal from './assets/petal.webp'
+
+const petals = Array.from({ length: 30 }, (_, i) => ({
+  id: i,
+  size: Math.floor(Math.random() * 11) + 10,
+  top: Math.random() * 100,
+  duration: Math.floor(Math.random() * 5) + 6,
+  delay: Math.random() * 8,
+}))
 
 function App() {
   return (
     <div className="relative w-full h-screen overflow-hidden">
       <img src={bg1} alt="background-1" className="absolute inset-0 w-full h-full object-cover" />
       <img src={bg2} alt="background-2" className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[190%] " />
-      <img src={frontScene} alt="background-2" className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[190%] " />
-      
+      <img src={cloud1} alt="cloud-1" className="absolute top-0 left-0 w-full" />
+      <img src={cloud2} alt="cloud-2" className="absolute top-0 left-0 w-full" />
+      <img src={cloud3} alt="cloud-3" className="absolute top-0 left-0 w-full" />
+      <img src={cloud4} alt="cloud-4" className="absolute top-0 left-0 w-full" />
+      {petals.map((p) => (
+        <img
+          key={p.id}
+          src={petal}
+          alt="petal"
+          className="petal"
+          style={{
+            width: p.size,
+            height: p.size,
+            top: `${p.top}%`,
+            right: '-3%',
+            animationDuration: `${p.duration}s`,
+            animationDelay: `${p.delay}s`,
+          }}
+        />
+      ))}
+      <img src={frontScene} alt="front-scene" className="absolute bottom-0 left-[50%] translate-x-[-50%] w-[190%] " />
     </div>
   )
 }
