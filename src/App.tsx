02f571049d557b3c752e130ec127675654af776d
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-26 15:02:21 -0500

    feat: setup React + Tailwind CSS with full-screen background-1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import bg1 from './assets/background-1.jpg'
+
+function App() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <img src={bg1} alt="background" className="w-full h-full object-cover" />
+    </div>
+  )
+}
+
+export default App
